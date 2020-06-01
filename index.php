067be38fe19eefa362b9133d30e4799b41a24448
Johan Solano <johanmsolanoc@gmail.com>
2020-05-31 23:23:40 -0400

    Eliminacion de script local

--- index.php
+++ index.php
@@ -578,7 +578,7 @@
         </div>
       </div>
     </div>
-    <script type="text/javascript" src="materialize.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script type="text/javascript">
       function continuar() {
         var estado = document.getElementsByTagName("input");
